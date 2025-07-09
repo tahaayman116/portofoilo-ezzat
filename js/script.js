e79b43c0bdd6e179426f19f59cc3a7286d3726c2
taha <tahaayman116@gmail.com>
2025-07-10 00:33:34 +0300

    feat: Make website fully responsive

--- js/script.js
+++ js/script.js
@@ -41,6 +41,31 @@ staggerAnimation('.certifications ul li', 100);
 staggerAnimation('.skill-item', 100);
 
 // Super-Modern Magnetic Cursor Logic
+const navSlide = () => {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    hamburger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        
+        // Hamburger Animation
+        hamburger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
+
 document.addEventListener('DOMContentLoaded', () => {
     // Super-Modern Magnetic Cursor Logic
     const cursor = document.querySelector('.cursor');
